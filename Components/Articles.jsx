1edd1c07106fab78ce03e23513f67c15b24c65d9
Jamie <jamiec145@gmail.com>
2025-04-02 15:56:56 +0100

    Articles Fetched

--- Components/Articles.jsx
+++ Components/Articles.jsx
@@ -1,8 +1,24 @@
 import Header from "./Header";
-function Articles(){
+import Footer from "./Footer";
+function Articles({articles}){
+    console.log("Articles received:", articles);
     return (
+        
     <>
     <Header/>
+    
+    <main>
+        <h2>Articles</h2>
+        <ul>
+          {articles.map((article) => (
+            <li key={article.article_id}>
+              <h3>{article.title}</h3>
+              <p>Author: {article.author}</p>
+              <p>{article.body}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
     <Footer/>
     </>
     );}
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import axios from "axios";
 import Header from "../Components/Header"; 
 import Footer from "../Components/Footer"; 
 import Topics from "../Components/Topics"; 
@@ -6,12 +9,27 @@ import Articles from "../Components/Articles";
 import Card from "../Components/Card"
 
 function App() {
+  
+  const [articles, setArticles] = useState([]);
+  
+  useEffect(() => {
+    axios.get("https://nc-action-news.onrender.com/api/articles")
+      .then(response => {
+        setArticles(response.data.articles);
+      })
+      .catch(error => {
+        console.error("Error fetching articles:", error);
+      });
+  }, []);
+  
+  
+  
   return (
     <div>
       <Header />
       <Routes>
         <Route path="/topics" element={<Topics />} />
-        <Route path="/articles" element={<Articles />} />
+        <Route path="/articles" element={<Articles articles={articles}/>} />
       </Routes>
       <Card  article="Article Name"/> <br />
       <Card article="Article Name"/>
